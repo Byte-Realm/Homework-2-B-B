51ad2965bb77f9b358d6d6e79904a07794fef4c0
chai-levy <chai9levy@gmail.com>
2024-11-20 01:37:21 +0200

    Add files via upload

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraSizeAdjuster : MonoBehaviour
+{
+    private Camera mainCamera;
+    [SerializeField]
+    private float targetObjectSize = 5f;
+    [Tooltip("Adjust for desired object size")]
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        float aspectRatio = (float)Screen.width / Screen.height;
+        float halfHeight = targetObjectSize / 2f;
+
+        if (aspectRatio >= 1) // (16:9) for an example
+        {
+            mainCamera.orthographicSize = halfHeight / aspectRatio;
+        }
+        else // (9:16) for an example
+        {
+            mainCamera.orthographicSize = halfHeight;
+        }
+    }
+}
